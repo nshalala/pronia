06a7a498105931eeb554cc71aacb42adae906d7d
Shalala <nasirlishalala1011@gmail.com>
2023-07-29 15:12:00 +0400

    initial commit

---
+++ Pronia/DataAccess/ProniaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pronia.Models;
+
+namespace Pronia.DataAccess
+{
+    public class ProniaDbContext:DbContext
+    {
+        public DbSet<Slider> Sliders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=SHALALA\\SADB;Database=Pronia;Trusted_Connection=True");
+        }
+    }
+}
---
+++ Pronia/Models/Slider.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pronia.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(30)]
+        public string Title { get; set; }
+        [Required, MaxLength(200)]
+        public string Description { get; set; }
+        [MaxLength(50)]
+        public string Offer { get; set; }
+        [Required, MaxLength(50)]
+        public string ButtonText { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+    }
+}
